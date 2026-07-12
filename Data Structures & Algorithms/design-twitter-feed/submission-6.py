b970e4087e4a79b157d45d6ca203e0fdaacefba2
Abdul Muyiz <43236045+abdulmuyiz@users.noreply.github.com>
2026-07-12 18:15:18 +0530

    Add: design-twitter-feed - submission-6

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-6.py
@@ -0,0 +1,47 @@
+class Twitter:
+
+    def __init__(self):
+        self.recent = 0
+        self.userId = 0
+        self.followlist = {}
+        self.tweet = {}
+        
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.userId = userId
+        self.recent += 1
+        if userId in self.tweet:
+            self.tweet[userId].append([self.recent,tweetId])
+        else:
+            self.tweet[userId] = [[self.recent,tweetId]]
+        
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        res = []
+        feed = [] if userId not in self.tweet else self.tweet[userId].copy()
+        if userId in self.followlist:
+            for i in self.followlist[userId]:
+                if i in self.tweet and userId != i:
+                    feed += self.tweet[i].copy()
+
+        heapq.heapify_max(feed)
+        print(feed)
+        count = 10
+        while feed and count:
+            count -= 1
+            res.append(heapq.heappop_max(feed)[1])
+
+        return res
+        
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.followlist:
+            if followeeId not in self.followlist[followerId]:
+                self.followlist[followerId].append(followeeId)
+        else:
+            self.followlist[followerId] = [followeeId]
+        
+
+    def unfollow(self, followerId: int, followeeId: int) -> None: 
+        if followerId in self.followlist and followeeId in self.followlist[followerId]:
+            self.followlist[followerId].remove(followeeId)
